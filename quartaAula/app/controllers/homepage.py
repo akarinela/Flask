7b47765cb1e28a443ae1d6f65e5cd9a8c499591a
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-10-29 10:33:22 -0300

    Add files via upload

---
+++ quartaAula/app/controllers/homepage.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+
+# @app.route("/")
+# def olaMundo():
+#    nome = "Xico"
+#    return render_template("index.html", nome=nome)
+
+# @app.route("/frutas")
+# def minhasfrutas():
+#    listas = ["uva", "maça", "abacaxi", "limão"]
+#    return render_template("frutas.html", listas=listas)
+
